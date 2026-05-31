31a3bc0c2c2bf954182ed82a4bbb6410d8b04bd3
rkwm19 <ruturajkademani19@gmail.com>
2026-06-01 02:26:12 +0530

    Webhook test

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ for _, r in show_df.iterrows():
 def call_matcher(uploaded, desc, dataset):
     try:
         return find_matches(uploaded, desc, dataset)
-    except Exception:
+    except Exception: # webhook test
         pass
     try:
         tmp = "tmp_upload.jpg"
